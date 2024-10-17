86f7904c1b9252a644c4b0345dead5cd3105d041
hwanbit <hwanbit@gmail.com>
2024-10-17 09:45:43 +0900

    업캐스팅 : 실습 10-4 (ArrayList)

--- java1017/src/MainShape.java
+++ java1017/src/MainShape.java
@@ -1,3 +1,6 @@
+import java.sql.Array;
+import java.util.ArrayList;
+
 public class MainShape {
 
     public static void main(String[] args) {
@@ -12,10 +15,18 @@ public class MainShape {
 
         // 참조변수의 선언을 부모 클래스로 하면 업캐스팅 => 참조의 범위가 달라진다
         // Shape s1 = new Square();
-        Shape[] shapes = {s, t, c};
 
-        for (int i=0; i < shapes.length; i++) {
-            System.out.printf("Index %d 번의 도형 이름 : %s\n", i, shapes[i].name);
+        // ArrayList로 자식 객체 관리
+        ArrayList <Shape> shapes = new ArrayList<Shape>();
+        shapes.add(s);
+        shapes.add(t);
+        shapes.add(c);
+
+        // 배열로 자식 객체 관리
+        //Shape[] shapes = {s, t, c};
+
+        for (int i=0; i < shapes.size(); i++) {
+            System.out.printf("Index %d 번의 도형 이름 : %s\n", i, shapes.get(i).name);
         }
     }
 }
